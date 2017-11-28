cc1370f16f839f7f62643f870d22dfcb4435d3d1
Johnson Denen <johnson.denen@gmail.com>
2017-11-28 09:07:41 -0500

    Use inconsolata font

--- init.el
+++ init.el
@@ -145,8 +145,8 @@ It should only modify the values of Spacemacs settings."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Droid Sans Mono"
-                               :size 13
+   dotspacemacs-default-font '("inconsolata"
+                               :size 16
                                :weight normal
                                :width normal
                                :powerline-scale 1.1)
