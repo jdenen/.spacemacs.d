d17549ecbfbd4407a1d63334df8442f7ea57a568
Johnson Denen <johnson.denen@gmail.com>
2017-11-28 09:06:59 -0500

    Update packages and layers

--- init.el
+++ init.el
@@ -9,7 +9,7 @@ This function should only modify configuration layer settings."
    ;; Base distribution to use. This is a layer contained in the directory
    ;; `+distribution'. For now available distributions are `spacemacs-base'
    ;; or `spacemacs'. (default 'spacemacs)
-   dotspacemacs-distribution 'spacemacs
+   dotspacemacs-distribution 'spacemacs-base
    ;; Lazy installation of layers (i.e. layers are installed only when a file
    ;; with a supported type is opened). Possible values are `all', `unused'
    ;; and `nil'. `unused' will lazy install only unused layers (i.e. layers
@@ -37,13 +37,14 @@ This function should only modify configuration layer settings."
      ;; ----------------------------------------------------------------
      better-defaults
      clojure
-     ;docker
+     docker
      emacs-lisp
      git
      ivy
      markdown
      org
      osx
+     (ruby :variables ruby-test-runner 'rspec ruby-version-manager 'rbenv)
      (shell :variables shell-default-shell 'shell)
      syntax-checking
      vinegar
